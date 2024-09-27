6905280d73972aad3ffd890e5d11af096c6390ab
Kaser <Kaser@students.uni-marburg.de>
2024-09-27 13:11:14 +0200

    Creating Models/Database & Adding simple home page view

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = 'database.sqlite3'
+
+def create_database():
+    db.create_all()
+    print('Database Created')
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'SLGDFMKADSF ASDQEWTY DFMLK'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -13,4 +23,6 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/auth') #go to localhost:5000/auth/login (for example)
     app.register_blueprint(admin, url_prefix='/')
 
+    with app.app_context():
+        create_database()
     return app
