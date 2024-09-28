91e2f4009b3e8004c631baf38da1f2d76970bb48
Kaser <Kaser@students.uni-marburg.de>
2024-09-28 08:22:01 +0200

    Adding Cart section

--- website/views.py
+++ website/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, flash, redirect, request, jsonify
 from .models import Product, Cart, Order
 from flask_login import login_required, current_user
+from . import db
+
 
 views = Blueprint('views', __name__)
 
@@ -9,4 +11,48 @@ def home():
 
     items = Product.query.filter_by(flash_sale=True)
 
-    return render_template('home.html', items=items)
+    return render_template('home.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                           if current_user.is_authenticated else [])
+
+
+@views.route('/add-to-cart/<int:item_id>')
+@login_required
+def add_to_cart(item_id):
+    item_to_add = Product.query.get(item_id)
+    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
+    if item_exists:
+        try:
+            item_exists.quantity = item_exists.quantity + 1
+            db.session.commit()
+            flash(f' Quantity of { item_exists.product.product_name } has been updated')
+            return redirect(request.referrer)
+        except Exception as e:
+            print('Quantity not Updated', e)
+            flash(f'Quantity of { item_exists.product.product_name } not updated')
+            return redirect(request.referrer)
+
+    new_cart_item = Cart()
+    new_cart_item.quantity = 1
+    new_cart_item.product_link = item_to_add.id
+    new_cart_item.customer_link = current_user.id
+
+    try:
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash(f'{new_cart_item.product.product_name} added to cart')
+    except Exception as e:
+        print('Item not added to cart', e)
+        flash(f'{new_cart_item.product.product_name} has not been added to cart')
+
+    return redirect(request.referrer)
+
+
+@views.route('/cart')
+@login_required
+def show_cart():
+    cart = Cart.query.filter_by(customer_link=current_user.id).all()
+    amount = 0
+    for item in cart:
+        amount += item.product.current_price * item.quantity
+
+    return render_template('cart.html', cart=cart, amount=amount, total=amount+200)
