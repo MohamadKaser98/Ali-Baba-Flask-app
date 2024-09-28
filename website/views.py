48a196e31a54dba74cc8d0dcc3fe7b5608f94423
Kaser <Kaser@students.uni-marburg.de>
2024-09-28 07:23:28 +0200

    Adding Admin Section with CRUD operations to the Items

--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, request, jsonify
+from .models import Product, Cart, Order
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+
+    items = Product.query.filter_by(flash_sale=True)
+
+    return render_template('home.html', items=items)
